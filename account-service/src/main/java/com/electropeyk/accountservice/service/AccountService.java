527fda84c5ca7515bcf77c990f2efd206461f1ca
alimahdavi <alijob675@gmail.com>
2019-11-12 00:49:24 +0330

    account service

---
+++ account-service/src/main/java/com/electropeyk/accountservice/service/AccountService.java
@@ -0,0 +1,15 @@
+package com.electropeyk.accountservice.service;
+
+import com.marcusdacoregio.accountservice.dto.UserDto;
+import com.marcusdacoregio.accountservice.dto.UserRegistrationDto;
+
+public interface AccountService {
+
+    /**
+     * Invokes Auth Service user creation
+     *
+     * @param user
+     * @return created account
+     */
+    UserDto create(UserRegistrationDto user);
+}
---
+++ config-server/src/main/java/com/electropeyk/configserver/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.electropeyk.configserver;
+
+public class SecurityConfig {
+}
